6b14a25a357c490e013b7aeeae64d25f4a370df0
Luiza <lumorgado@id.uff.br>
2022-10-27 22:20:44 -0300

    Centralized fields

--- Login.java
+++ Login.java
@@ -4,18 +4,18 @@ import java.awt.event.*;
 
 public class Login {
   public static void main(String[] args) {
-    JFrame f = new JFrame("Button Example");
+    JFrame f = new JFrame("Login");
     
     final JTextField email = new JTextField();
-    email.setBounds(50, 50, 150, 20);
+    email.setBounds(125, 50, 150, 20);
     email.setText("email@email.com");
 
     final JTextField senha = new JTextField();
-    senha.setBounds(50, 75, 150, 20);
+    senha.setBounds(125, 75, 150, 20);
     senha.setText("*******");
 
     JButton b = new JButton("Entrar");
-    b.setBounds(50, 100, 95, 30);
+    b.setBounds(180, 100, 95, 30);
     b.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
         email.setText("Welcome to Javatpoint.");
